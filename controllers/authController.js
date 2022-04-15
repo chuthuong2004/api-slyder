f83bbcbcef07ade1d50dba4b4f77accafc1e533d
chuthuong2004 <19h1120035@sv.ut.edu.vn>
2022-04-15 23:20:04 +0700

    hello

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ const generateAccessToken = (user) => {
     return jwt.sign({
         id: user.id,
         isAdmin: user.isAdmin,
-    }, process.env.JWT_ACCESS_KEY, { expiresIn: '1d' });
+    }, process.env.JWT_ACCESS_KEY, { expiresIn: '30s' });
 }
 
 // GENERATE REFRESH TOKEN
