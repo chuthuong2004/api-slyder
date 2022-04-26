1ef6d09e4d9dfd95bbbd88a13db508d2c2f9a233
chuthuong2004 <19h1120035@sv.ut.edu.vn>
2022-04-26 19:36:43 +0700

    sfdg

--- controllers/middlewareController.js
+++ controllers/middlewareController.js
@@ -132,7 +132,10 @@ const middlewareController = {
     isRequestValidated: (req, res, next) => {
         const errors = validationResult(req)
         if (errors.array().length > 0) {
-            return res.status(400).json({ errors: errors.array()[0].msg })
+            return res.status(400).json({
+                success: false,
+                message: errors.array()[0].msg
+            })
         } else {
             next();
         }
