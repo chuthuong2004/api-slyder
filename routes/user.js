0824e53565d3a10d1f45cee1c323c89d3c0c01bc
chuthuong2004 <19h1120035@sv.ut.edu.vn>
2022-12-06 21:46:28 +0700

    123

--- routes/user.js
+++ routes/user.js
@@ -9,74 +9,73 @@ import path from "path";
 import { fileURLToPath } from "url";
 import moment from "moment";
 
-const __filename = fileURLToPath(
-    import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const date = moment(Date.now()).format("yyyyMMDDhhmmss");
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, path.join(path.dirname(__dirname), "uploads/avatars"));
-    },
-    filename: function(req, file, cb) {
-        cb(null, date + "-" + file.originalname);
-    },
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), "uploads/avatars"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, date + "-" + file.originalname);
+  },
 });
 
 const upload = multer({ storage });
 
 // * GET ALL USER ---- Admin
 router.get(
-    "/admin/users",
-    middlewareController.verifyTokenAndAdminAuth,
-    userController.getAllUser
+  "/admin/users",
+  middlewareController.verifyTokenAndAdminAuth,
+  userController.getAllUser
 );
 
 // * GET A USER ---- Admin
 router.get(
-    "/admin/user/:id",
-    middlewareController.verifyTokenAndAdminAuth,
-    userController.getUser
+  "/admin/user/:id",
+  middlewareController.verifyTokenAndAdminAuth,
+  userController.getUser
 );
 
 // * UPDATE USER ROLE ----- Admin
 router.put(
-    "/admin/user/:id",
-    middlewareController.verifyTokenAndAdminAuth,
-    userController.updateUserRole
+  "/admin/user/:id",
+  middlewareController.verifyTokenAndAdminAuth,
+  userController.updateUserRole
 );
 
 // * DELETE USER ----- Admin
 router.delete(
-    "/admin/user/:id",
-    middlewareController.verifyTokenAndAdminAuth,
-    userController.deleteUser
+  "/admin/user/:id",
+  middlewareController.verifyTokenAndAdminAuth,
+  userController.deleteUser
 );
 
 // * GET USER DETAILS
 router.get("/me", middlewareController.verifyToken, userController.getProfile);
 
-// * UPDATE USER
+// * UPDATE USERs
 router.put(
-    "/me/update",
-    middlewareController.verifyToken,
-    upload.single("avatar"),
-    userController.updateProfile
+  "/me/update",
+  middlewareController.verifyToken,
+  upload.single("avatar"),
+  userController.updateProfile
 );
 
 // * FORGOT PASSWORD
 router.post(
-    "/password/forgot",
-    middlewareController.validateEmail,
-    middlewareController.isRequestValidated,
-    userController.forgotPassword
+  "/password/forgot",
+  middlewareController.validateEmail,
+  middlewareController.isRequestValidated,
+  userController.forgotPassword
 );
 
 // * CHANGE PASSWORD
 router.post(
-    "/password/update",
-    middlewareController.verifyToken,
-    middlewareController.validateChangePassword,
-    middlewareController.isRequestValidated,
-    userController.changePassword
+  "/password/update",
+  middlewareController.verifyToken,
+  middlewareController.validateChangePassword,
+  middlewareController.isRequestValidated,
+  userController.changePassword
 );
 export default router;
